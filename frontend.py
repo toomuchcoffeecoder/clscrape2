d5d91afcdcc3f907fbfd5dea777051a172822f61
Tim McDonald <tim@spacestation-tim.com>
2016-12-13 16:07:56 -0600

    deleted old unused code

--- frontend.py
+++ frontend.py
@@ -1,8 +1,5 @@
-from pprint import pformat, pprint
-
 from flask import Flask, Response, request
 from flask import render_template, url_for, jsonify
-from flask_login import LoginManager, UserMixin, login_required
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,6 +7,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+mysqlconnector://'
                                         'tools:YPrn3Uy8OX61DQlFjinb'
                                         '@localhost/cl_scrape')
+# set SQLALCHEMY_POOL_RECYCLE to 10 (seconds?) less than mysql
+# wait_timeout which is currently 600
+# hopefully this will eleminate the connection error encounter
+# after the database sits idle
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 590
 db = SQLAlchemy(app)
 
 
@@ -39,44 +41,9 @@ class ClAd(db.Model):
         self.dt = dt
 
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-class User(UserMixin):
-    # proxy for a database of users
-    user_database = {"JohnDoe": ("JohnDoe", "John"),
-                   "JaneDoe": ("JaneDoe", "Jane")}
-
-    def __init__(self, username, password):
-        self.id = username
-        self.password = password
-
-        @classmethod
-        def get(cls,id):
-            return cls.user_database.get(id)
-
-
-@login_manager.request_loader
-def load_user(request):
-    token = request.headers.get('Authorization')
-    if token is None:
-        token = request.args.get('token')
-
-    if token is not None:
-        username,password = token.split(":") # naive token
-        user_entry = User.get(username)
-        if (user_entry is not None):
-            user = User(user_entry[0],user_entry[1])
-            if (user.password == password):
-                return user
-
-    return None
-
-@app.route('/', methods=['GET'])
-def index():
-    response = 'Place holder'
-    return Response(response=response, status=200)
+@app.route('/cl_ads_table', methods=['GET'])
+def cl_table_main():
+    return render_template('cl_ads_tbl.html')
 
 @app.route('/cl_ads_list', methods=['POST'])
 def cl_ad_list():
@@ -111,17 +78,6 @@ def cl_ad_list():
         result['Records'].append(row)
 
     return jsonify(result)
-
-
-@app.route('/cl_ads_table', methods=['GET'])
-def cl_table_main():
-    return render_template('cl_ads_tbl.html')
-
-@app.route('/protected/', methods=['GET'])
-@login_required
-def protected():
-    return Response(response='Hello Protected World', status=200)
-
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',
